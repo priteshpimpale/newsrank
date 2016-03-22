55b054d403983f13204ea4e02fbd338ec4512acf
priteshpimpale <priteshpimpale@gmail.com>
2016-03-21 18:36:08 -0700

    Demo 1

--- JS/index.js
+++ JS/index.js
@@ -9,61 +9,113 @@
 var userInfo = null;
 
 function postQuestion() {
+    
+    
+    
     var headline = $("#headline").val();
     var content = $("#newsContent").val();
     var url = $("#newsURL").val();
-    $.ajax({
-        url: "http://localhost:3000/allnews",
-        type: "POST",
-        data: JSON.stringify({ "headLine": headline.toString(), "content": content.toString(), "url": url.toString(), "votes": 0 }),
-        contentType: "application/json",
-        success: function(response) {
-            console.log("Successful Insert");
-        },
-        error: function(error) {
-            console.error("Error : " + error.status);
-        }
-    });
+    
+    if(headline !== "" && content !== ""){
+        $.ajax({
+            url: "/allnews",
+            type: "POST",
+            data: JSON.stringify({ "headLine": headline.toString(), "content": content.toString(), "url": url.toString(), "votes": 0 , "date" : new Date() }),
+            contentType: "application/json",
+            success: function(response) {
+                console.log("Successful Insert");
+                response.up = "black-text";
+                response.down = "black-text";
+                var card = createCard(response);
+                $("#topnews")[0].innerHTML += card;
+                $("modal1").closeModal();
+                
+            },
+            error: function(error) {
+                console.error("Error : " + error.status);
+            }
+        });
+    }else{
+        $("#postError").toggleClass("hide",false);
+    }
 }
 
-function loadNews() {
+function loadNews(section) {
+    var newsDiv,sort,url,querystring="";
+    if(section === "top" ){
+        newsDiv = "#topnews";
+        url = "/allnews/?_sort=votes&_order=DESC";
+    }else if(section === "latest"){
+        newsDiv = "#latestnews";
+        url = "/allnews/?_sort=date&_order=DESC";
+    }else if(section === "liked"){
+        newsDiv = "#likednews";
+        $.each(userInfo.votes.up,function(i,item){
+            querystring += "id="+item+"&";
+        });
+        url = "/allnews/?"+(querystring === "" ? "id=black":querystring);
+    }else if(section === "disliked"){
+        newsDiv = "#dislikednews";
+        $.each(userInfo.votes.down,function(i,item){
+            querystring += "id="+item+"&";
+        });
+        url = "/allnews/?" + (querystring === "" ? "id=black":querystring);
+    }
+    
     $.ajax({
         type: "GET",
-        url: "http://localhost:3000/allnews",
+        url: url,
         datatype: "json",
         success: function(response) {
             console.log("Get all news successfull");
-            $.each(response, function(i, item) {
-                if (userInfo !== null) {
-                    item.up = (userInfo.votes.up.indexOf(item.id) != -1) ? "blue-text" : "black-text";
-                    item.down = (userInfo.votes.down.indexOf(item.id) != -1) ? "red-text" : "black-text";
-                }
-                else {
-                    item.up = "black-text";
-                    item.down = "black-text";
-                }
-                var card = "<div class=\"col s12 m6 l4\">" +
-                    "<div class=\"card hoverable\">" +
-                    "<div class=\"card-content\">" +
-                    "<span class=\"card-title truncate\">" + item.headLine + "</span>" +
-                    "<p>" + item.content + "</p>" +
-                    "<a href=\"" + item.url + "\">" + item.url + "</a>" +
-                    "</div>" +
-                    "<div data-id=\"" + item.id + "\" class=\"card-action right-align\">" +
-                    /*"<a><span>3</span><span> Comments</span></a>" +*/
-                    // "<a href=\""+ item.url +"\">"+ item.url +"</a>"+
-                    "<a class=\"waves-effect waves-teal btn-flat btn-floating  white thumbdown\"><i class=\"material-icons " + item.down + "\">thumb_down</i></a>" +
-                    "<span class=\"votes\">" + item.votes + "</span>" +
-                    "<a class=\"waves-effect waves-teal btn-flat btn-floating white thumbup\"><i class=\"material-icons " + item.up + "\">thumb_up</i></a>" +
-                    "</div>" +
-                    "</div>" + "</div>";
-                $("#topnews")[0].innerHTML += card;
-            });
+            if(section === "latest"){
+                response.sort(function(a,b){
+                    //console.log(new Date(a.date));
+                    return new Date(b.date) - new Date(a.date);
+                });
+            }
+            if(response.length > 0){
+                $.each(response, function(i, item) {
+                    console.log(new Date(item.date));
+                    if (userInfo !== null) {
+                        item.up = (userInfo.votes.up.indexOf(item.id) != -1) ? "blue-text" : "black-text";
+                        item.down = (userInfo.votes.down.indexOf(item.id) != -1) ? "red-text" : "black-text";
+                    }
+                    else {
+                        item.up = "black-text";
+                        item.down = "black-text";
+                    }
+                    var card = createCard(item);
+                    $(newsDiv)[0].innerHTML += card;
+                });
+            } else{
+                var cardnew = "<div class=\"row\"><div class=\"col s12 m12 l12 \"><div class=\"card-panel\">"+
+                    "<span class=\"black-text\"> Nothing to show </span></div></div></div>";
+                $(newsDiv)[0].innerHTML += cardnew;
+            }
         },
         error: function(xhr) {
             console.error("Error fetching news: " + xhr.status + "\n" + xhr.responseText);
         }
     });
+}
+
+
+var createCard = function(item){
+    return "<div class=\"col s12 m6 l4\">" +
+                "<div class=\"card hoverable\" data-activates=\"read-news-slide\">" +
+            "<div class=\"card-content\">" +
+            "<span class=\"card-title truncate\">" + item.headLine + "</span>" +
+            "<p>" + item.content + "</p>" +
+            "<a target=\"_blank\" class=\"truncate\" href=\"" + item.url + "\">" + item.url + "</a>" +
+            "</div>" +
+            "<div data-id=\"" + item.id + "\" class=\"card-action right-align\">" +
+            /*"<a><span>3</span><span> Comments</span></a>" +*/
+            // "<a href=\""+ item.url +"\">"+ item.url +"</a>"+
+            "<a class=\"waves-effect waves-teal btn-flat btn-floating  white thumbdown\"><i class=\"material-icons " + item.down + "\">thumb_down</i></a>" +
+            "<span class=\"votes\">" + item.votes + "</span>" +
+            "<a class=\"waves-effect waves-teal btn-flat btn-floating white thumbup\"><i class=\"material-icons " + item.up + "\">thumb_up</i></a>" +
+            "</div></div></div>";
 }
 
 var thumbupClick = function() {
@@ -147,7 +199,7 @@ var thumbdownClick = function() {
 function updateNewsVotes(newsId, votes) {
     $.ajax({
         type: "PATCH",
-        url: "http://localhost:3000/allnews/" + newsId,
+        url: "/allnews/" + newsId,
         data: JSON.stringify({ "votes": votes }),
         contentType: "application/json",
         datatype: "json",
@@ -161,15 +213,9 @@ function updateNewsVotes(newsId, votes) {
 }
 
 function updateUserVotes() {
-    var updateArray;
-    /*if(updown === "up"){
-        updateArray = userInfo.votes.up;
-    } else if (updown === "down"){
-        updateArray = userInfo.votes.down;
-    }*/
     $.ajax({
         type: "PATCH",
-        url: "http://localhost:3000/users/" + userInfo.id,
+        url: "/users/" + userInfo.id,
         data: JSON.stringify({ "votes": userInfo.votes }),
         contentType: "application/json",
         datatype: "json",
@@ -187,20 +233,31 @@ function authenticateUser() {
     var password = $("#sipassword")[0].value;
     $("#modalsigninerror").addClass("hide");
     $.ajax({
-        url: "http://localhost:3000/users/?username=" + userName + "&password=" + password,
+        url: "/users/?username=" + userName + "&password=" + password,
         type: "GET",
         contentType: "application/json",
         //data: JSON.stringify({"username": userName.toString()}),
         success: function(response) {
             console.log(response);
             console.log(response.length);
+            
             if (response.length === 0) {
                 $("#modalsigninerror").toggleClass("hide");
             }
             else if (response.length === 1) {
                 userInfo = response[0];
+                loadNews("liked");
+                loadNews("disliked");
                 $("#signinSuccess").toggleClass("hide");
-                $("#usernameTitleBar")[0].innerHTML = userInfo.username;
+                $("#usernameTitleBar")[0].innerHTML = userInfo.username + "<i class=\"material-icons right\">arrow_drop_down</i>";
+                $("#musernameTitleBar")[0].innerHTML = userInfo.username + "<i class=\"material-icons right\">arrow_drop_down</i>";
+                $("#usernameTitleBar,#musernameTitleBar").toggleClass("hide",false);
+                $("#mode1,#mmode1").toggleClass("hide",false); 
+                $("#mode2,#mmode2,#mode3,#mmode3").toggleClass("hide",true);
+                $("#tabliked,#tabdisliked").toggleClass("hide",false);
+                $("#tabs").find("li.tabs a.active").toggleClass("active",false);
+                $("#tabtop").find("a").toggleClass("active",true);
+                //$("").toggleClass("hide",false);
                 setTimeout(function() {
                     $('#modalsignin').closeModal();
                     $("#signinSuccess").toggleClass("hide");
@@ -255,7 +312,7 @@ function checkUserName() {
     var userName = $("#userName").val();
     $("#errorAlert").addClass("hide");
     $.ajax({
-        url: "http://localhost:3000/users/?username=" + userName,
+        url: "/users/?username=" + userName,
         type: "GET",
         contentType: "application/json",
         data: JSON.stringify({ "username": userName.toString() }),
@@ -281,14 +338,17 @@ function registerUser() {
     var password = $("#password").val();
     var votes = { "up": [], "down": [] };
     $.ajax({
-        url: "http://localhost:3000/users",
+        url: "/users",
         type: "POST",
         data: JSON.stringify({ "username": userName.toString(), "password": password.toString(), "votes": votes }),
         contentType: "application/json",
         success: function(response) {
             $("#regSuccess").toggleClass("hide");
             userInfo = response;
-            $("#usernameTitleBar")[0].innerHTML = userInfo.username;
+            $("#usernameTitleBar,#musernameTitleBar")[0].innerHTML = userInfo.username + + "<i class=\"material-icons right\">arrow_drop_down</i>"; 
+            $("#usernameTitleBar,#musernameTitleBar").toggleClass("hide",false);
+            $("#mode1,#mmode1").toggleClass("hide",false);
+            $("#mode2,#mmode2,#mode3,#mmode3").toggleClass("hide",true);
             updateupdownvotesonscreen(true);
             setTimeout(function() {
                 $('#modal3').closeModal();
@@ -302,6 +362,21 @@ function registerUser() {
     });
 }
 
+var searchNews = function(){
+    var query = $("#search")[0].value;
+    var cards = $("#topnews").find("div.col");
+    cards = $.merge(cards,$("#latestnews").find("div.col"));
+    $.each(cards, function(i, card) {
+        var content = $(card).find("div.card div.card-content p")[0].innerHTML.toLowerCase();
+        var header  = $(card).find("div.card div.card-content span.card-title")[0].innerHTML.toLowerCase();
+        if (content.indexOf(query) === -1 && header.indexOf(query) === -1){
+            $(this).toggleClass("hide",true);
+        }else{
+            $(this).toggleClass("hide",false);
+        }
+    });
+};
+
 
 $(document).ready(function() {
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
@@ -311,9 +386,16 @@ $(document).ready(function() {
 
     $("#mode1,#mmode1").on("click", function() {
         $('#modal1').openModal();
+        $("#postError").toggleClass("hide",true);
     });
     
     $(".button-collapse").sideNav();
+    
+    $(".cardl").sideNav({
+      menuWidth: 500, // Default is 240
+      edge: 'right', // Choose the horizontal origin
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    });
 
     $("#mode2,#mmode2").on("click", function() {
         $("#siuserName")[0].value = "";
@@ -325,15 +407,22 @@ $(document).ready(function() {
         $('#modalsignin').openModal();
     });
 
-    $("#topnews").on("click", "div.col div.card div.card-action a.thumbup", thumbupClick);
-    $("#topnews").on("click", "div.col div.card div.card-action a.thumbdown", thumbdownClick);
+    $("#topnews,#latestnews,#likednews,#dislikednews").on("click", "div.col div.card div.card-action a.thumbup", thumbupClick);
+    $("#topnews,#latestnews,#likednews,#dislikednews").on("click", "div.col div.card div.card-action a.thumbdown", thumbdownClick);
 
-    $("#signout").on("click", function() {
+    $("#signout,#msignout").on("click", function() {
+        $('.button-collapse').sideNav('hide');
         userInfo = null;
-        $("#usernameTitleBar")[0].innerHTML = "";
+        $("#usernameTitleBar,#musernameTitleBar")[0].innerHTML = "";
+        $("#usernameTitleBar,#musernameTitleBar").toggleClass("hide",true);
+        $("#mode1,#mmode1").toggleClass("hide",true);
+        $("#mode2,#mmode2,#mode3,#mmode3").toggleClass("hide",false);
+        $("#tabliked,#tabdisliked").toggleClass("hide",true);
         updateupdownvotesonscreen(false);
         Materialize.toast('Successfully Signed out.', 2000);
     });
-
-    loadNews();
+    
+    $("#search").on("change",searchNews);
+    loadNews("top");
+    loadNews("latest");
 });
